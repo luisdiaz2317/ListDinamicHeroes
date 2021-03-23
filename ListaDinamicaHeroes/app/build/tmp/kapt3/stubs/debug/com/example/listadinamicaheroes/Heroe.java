00a3df8c825b34815a5544eba80c44355176840d
@luis.diaz.agudelo <luis.diaz.agudelo@correonille.edu.co>
2021-03-23 13:03:14 -0500

    Homework 2 complete

---
+++ ListaDinamicaHeroes/app/build/tmp/kapt3/stubs/debug/com/example/listadinamicaheroes/Heroe.java
@@ -0,0 +1,52 @@
+package com.example.listadinamicaheroes;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 1, 16}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u000e\n\u0002\b\r\u0018\u00002\u00020\u0001B\u001d\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0003\u0012\u0006\u0010\u0005\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0006J\u0006\u0010\u000f\u001a\u00020\u0003R\u001a\u0010\u0004\u001a\u00020\u0003X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0007\u0010\b\"\u0004\b\t\u0010\nR\u001a\u0010\u0002\u001a\u00020\u0003X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u000b\u0010\b\"\u0004\b\f\u0010\nR\u001a\u0010\u0005\u001a\u00020\u0003X\u0086\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\r\u0010\b\"\u0004\b\u000e\u0010\n\u00a8\u0006\u0010"}, d2 = {"Lcom/example/listadinamicaheroes/Heroe;", "", "name", "", "alterEgo", "url", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V", "getAlterEgo", "()Ljava/lang/String;", "setAlterEgo", "(Ljava/lang/String;)V", "getName", "setName", "getUrl", "setUrl", "getFullName", "app_debug"})
+public final class Heroe {
+    @org.jetbrains.annotations.NotNull()
+    private java.lang.String name;
+    @org.jetbrains.annotations.NotNull()
+    private java.lang.String alterEgo;
+    @org.jetbrains.annotations.NotNull()
+    private java.lang.String url;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getFullName() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getName() {
+        return null;
+    }
+    
+    public final void setName(@org.jetbrains.annotations.NotNull()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getAlterEgo() {
+        return null;
+    }
+    
+    public final void setAlterEgo(@org.jetbrains.annotations.NotNull()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getUrl() {
+        return null;
+    }
+    
+    public final void setUrl(@org.jetbrains.annotations.NotNull()
+    java.lang.String p0) {
+    }
+    
+    public Heroe(@org.jetbrains.annotations.NotNull()
+    java.lang.String name, @org.jetbrains.annotations.NotNull()
+    java.lang.String alterEgo, @org.jetbrains.annotations.NotNull()
+    java.lang.String url) {
+        super();
+    }
+}
